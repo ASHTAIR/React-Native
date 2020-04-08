f5630ab3837237555ccfa5839d3b20ed5c321e30
ASHTAIR <ashtair1001@gmail.com>
2020-04-08 09:37:54 +0200

    Componentes React Native

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Campobase from './componentes/CampobaseComponent';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Campobase />
+    );
+  }
+}
+
