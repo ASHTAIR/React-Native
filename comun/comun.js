5208d1df01856a3d1bf505af9ec7fcd049434eeb
ASHTAIR <36845187+ASHTAIR@users.noreply.github.com>
2020-05-04 08:47:18 +0200

    Activity Indicator y addFavoritos

--- comun/comun.js
+++ comun/comun.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://192.168.1.129:3001/";
+export const baseUrl = "http://192.168.1.42:3001/";
 export const colorGaztaroaOscuro = '#015afc';
 export const colorGaztaroaClaro = '#c2d3da';
